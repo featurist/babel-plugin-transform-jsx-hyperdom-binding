0dc77078536836348fbf048954cde53e9f57a5de
Tim Macfarlane <timmacfarlane@gmail.com>
2017-02-22 07:09:09 +0000

    can transform jsx hyperdom bindings

---
+++ index.js
@@ -0,0 +1,23 @@
+var jsx = require('babel-plugin-syntax-jsx')
+var babylon = require('babylon')
+
+module.exports = function (babel) {
+  return {
+    inherits: jsx,
+
+    visitor: {
+      JSXAttribute: function (path) {
+        var namePath = path.get('name')
+        var valuePath = path.get('value')
+        if (valuePath.node.type == 'StringLiteral' && namePath.node.type == 'JSXIdentifier' && namePath.node.name == 'binding') {
+          var match = /^(.*)\.([a-z_][a-z0-9_]*$)/.exec(valuePath.node.value)
+          if (match) {
+            var parsed = babylon.parse(match[1])
+            var expression = parsed.program.body[0].expression
+            valuePath.replaceWith(babel.types.jSXExpressionContainer(babel.types.arrayExpression([expression, babel.types.stringLiteral(match[2])])))
+          }
+        }
+      },
+    }
+  };
+};
