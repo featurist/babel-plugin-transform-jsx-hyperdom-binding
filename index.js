1eeb974fbe075179aabc42d72ac2db7421595120
Tim Macfarlane <timmacfarlane@gmail.com>
2017-05-02 09:49:44 +0200

    allowing uppercase binding strings

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function (babel) {
         var namePath = path.get('name')
         var valuePath = path.get('value')
         if (valuePath.node && valuePath.node.type == 'StringLiteral' && namePath.node.type == 'JSXIdentifier' && namePath.node.name == 'binding') {
-          var match = /^(.*)\.([a-z_][a-z0-9_]*$)/.exec(valuePath.node.value)
+          var match = /^(.*)\.([a-z_][a-z0-9_]*$)/i.exec(valuePath.node.value)
           if (match) {
             var parsed = babylon.parse(match[1])
             var expression = parsed.program.body[0].expression
